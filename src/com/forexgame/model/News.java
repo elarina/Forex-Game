28b738d9518e282d132c9b472ce857b47d584f33
elarina <elena.larina.1@gmail.com>
2017-05-27 10:23:42 +0300

    Add dummy for the news

---
+++ src/com/forexgame/model/News.java
@@ -0,0 +1,32 @@
+package com.forexgame.model;
+
+public class News {
+	public String heading;
+	public String content;
+	public String date;
+	public String source;
+	public String getHeading() {
+		return heading;
+	}
+	public void setHeading(String heading) {
+		this.heading = heading;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
